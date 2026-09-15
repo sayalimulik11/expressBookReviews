d765f97d8456f68bb55ff2154bc40d375241dbfe
sayalimulik11 <sayalimulik11@gmail.com>
2026-09-15 13:34:46 +0530

    Add Task 5 - getbooksbytitle proof

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,11 +35,16 @@ public_users.get('/author/:author', function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  let filtered = {};
+  for (let isbn in books) {
+    if (books[isbn].title === title) {
+      filtered[isbn] = books[isbn];
+    }
+  }
+  res.send(JSON.stringify(filtered, null, 4));
 });
-
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
