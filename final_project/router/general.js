0f7f764f40d6b3610f6320af84c368da49752f90
sayalimulik11 <sayalimulik11@gmail.com>
2026-09-15 14:12:38 +0530

    Add Task 7 - register proof

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,12 +4,21 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  const username = req.body.username;
+  const password = req.body.password;
 
+  if (!username || !password) {
+    return res.status(404).json({message: "Username and password are required"});
+  }
+
+  if (users.some(user => user.username === username)) {
+    return res.status(404).json({message: "Username already exists"});
+  }
+
+  users.push({username: username, password: password});
+  return res.status(200).json({message: "User successfully registered. Now you can login"});
+});
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
   res.send(JSON.stringify(books, null, 4));
