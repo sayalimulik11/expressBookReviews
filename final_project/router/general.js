4b9d1121cd1fe67ec34b578aa760b4cf29f7feda
sayalimulik11 <sayalimulik11@gmail.com>
2026-09-15 13:14:06 +0530

    Add Task 4 - getbooksbyauthor proof

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,21 +11,27 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  res.send(JSON.stringify(books[isbn], null, 4));
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  let filtered = {};
+  for (let isbn in books) {
+    if (books[isbn].author === author) {
+      filtered[isbn] = books[isbn];
+    }
+  }
+  res.send(JSON.stringify(filtered, null, 4));
 });
 
 // Get all books based on title
