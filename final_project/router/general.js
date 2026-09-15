25dfd8953760164a09c4a7c62d2c7377475d9b7b
sayalimulik11 <sayalimulik11@gmail.com>
2026-09-15 15:06:34 +0530

    Add Task 11 - Axios promise and async/await implementations

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -24,7 +25,6 @@ public_users.get('/', function (req, res) {
   res.send(JSON.stringify(books, null, 4));
 });
 
-// Get book details based on ISBN
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
@@ -54,9 +54,53 @@ public_users.get('/title/:title', function (req, res) {
   }
   res.send(JSON.stringify(filtered, null, 4));
 });
+
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
   const isbn = req.params.isbn;
   res.send(JSON.stringify(books[isbn].reviews, null, 4));
 });
+
+// Task 10: Get all books using Promise callbacks
+function getAllBooksPromise() {
+  axios.get('http://localhost:5000/')
+    .then(response => {
+      console.log("All books (Promise):", response.data);
+    })
+    .catch(error => {
+      console.log("Error:", error.message);
+    });
+}
+
+// Task 11: Get book by ISBN using Promise callbacks
+function getBookByISBNPromise(isbn) {
+  axios.get(`http://localhost:5000/isbn/${isbn}`)
+    .then(response => {
+      console.log("Book by ISBN (Promise):", response.data);
+    })
+    .catch(error => {
+      console.log("Error:", error.message);
+    });
+}
+
+// Task 12: Get books by author using async/await
+async function getBooksByAuthorAsync(author) {
+  try {
+    const response = await axios.get(`http://localhost:5000/author/${author}`);
+    console.log("Books by Author (Async/Await):", response.data);
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
+}
+
+// Task 13: Get books by title using async/await
+async function getBooksByTitleAsync(title) {
+  try {
+    const response = await axios.get(`http://localhost:5000/title/${title}`);
+    console.log("Books by Title (Async/Await):", response.data);
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
+}
+
 module.exports.general = public_users;
