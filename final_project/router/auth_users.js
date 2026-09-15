68dbf3fd941fd91ecc7048ff263c2e74bef55200
sayalimulik11 <sayalimulik11@gmail.com>
2026-09-15 15:01:40 +0530

    Add Task 9 and 10 - review proofs

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -49,6 +49,23 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.status(200).json({message: "Review successfully added/updated", reviews: books[isbn].reviews});
 });
 
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+
+  if (!books[isbn]) {
+    return res.status(404).json({message: "Book not found"});
+  }
+
+  if (books[isbn].reviews[username]) {
+    delete books[isbn].reviews[username];
+    return res.status(200).json({message: "Review successfully deleted", reviews: books[isbn].reviews});
+  } else {
+    return res.status(404).json({message: "No review found for this user"});
+  }
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
