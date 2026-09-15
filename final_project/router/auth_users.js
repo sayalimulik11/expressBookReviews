3881e83f7cb5caa7e7cf8a98ba8229ccbd38b9a0
sayalimulik11 <sayalimulik11@gmail.com>
2026-09-15 14:54:32 +0530

    Add Task 9 - reviewadded proof

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -33,8 +33,20 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+  const username = req.session.authorization.username;
+
+  if (!books[isbn]) {
+    return res.status(404).json({message: "Book not found"});
+  }
+
+  if (!review) {
+    return res.status(404).json({message: "Review text is required"});
+  }
+
+  books[isbn].reviews[username] = review;
+  return res.status(200).json({message: "Review successfully added/updated", reviews: books[isbn].reviews});
 });
 
 module.exports.authenticated = regd_users;
